2dc76c0d12e0aad2a03983e1e0fc4766009c4b8d
Enting Cui <2968726c@student.gla.ac.uk>
2025-03-06 17:58:41 +0000

    Resolve conflicts in README.md and sbt.bat

--- README.md
+++ README.md
@@ -1,7 +1,2 @@
-<<<<<<< HEAD
-# ITSDTeamProject-0
-
-=======
-# ITSDTeamProject-0
-
->>>>>>> 25db6859853632acb7212d5609170410961505f5
+# Team Project
+Updated locally and from remote
--- sbt.bat
+++ sbt.bat
@@ -1,9 +1,2 @@
-<<<<<<< HEAD
-@REM SBT launcher script
-
-.\sbt-dist\bin\sbt.bat %*
-=======
-@REM SBT launcher script
-
-.\sbt-dist\bin\sbt.bat %*
->>>>>>> 25db6859853632acb7212d5609170410961505f5
+@echo off
+java -jar sbt-launch.jar %*
